4d42c79a2f08c266eabb0ebcaede690362408e67
Y-ash-Thakur <yasht0509@gmail.com>
2026-05-14 23:26:46 +0530

    req.body, req.params, req.query

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/api/health', (req, res) => {
     res.json({ 
         success: true,
@@ -16,25 +18,38 @@ app.get('/api/about', (req, res) => {
     })
 })
 
-app.get('/api/contact', (req, res) => {
+app.post('/api/test', (req, res) => {
+    console.log(req.body);
+    
     res.json({
-  id: 1,
-  name: "Alex Johnson",
-  username: "alexj_92",
-  email: "alex.johnson@example.com",
-  address: {
-    street: "123 Mockingbird Lane",
-    city: "Anytown",
-    zipcode: "90210"
-  },
-  phone: "555-010-9988",
-  website: "alexjohnson.dev",
-  company: {
-    name: "TechFlow Solutions",
-    catchPhrase: "Streamlining your digital future"
-  }
+        success: true,
+        receivedData: req.body,
+    })
 })
 
+app.post('/api/login', (req, res) => {
+    console.log(req.body);
+    
+    res.json({
+        success: true,
+        message: "Login Successful",
+    })
 })
+
+app.get('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    
+    res.json({noteId})
+})
+
+app.get('/api/search', (req, res) => {
+    const query = req.query.keyword;
+    
+    res.json({
+        "find": query,
+    })
+})
+
+// app.use(express.json());
 
 export default app;
